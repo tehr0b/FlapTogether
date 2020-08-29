7e0a6e95060d293696e347b3767c3526a28da856
Rob Giusti <therobgiusti@gmail.com>
2020-08-29 16:31:38 -0700

    Windmill & Rotator

--- Assets/Audio/AudioSourceExtension.cs
+++ Assets/Audio/AudioSourceExtension.cs
@@ -1,9 +1,12 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public static class AudioSourceExtension {
   public static void PlaySoundFromGroup(AudioClip[] clips) {
+    Debug.Log("Sound");
     if (clips.Length > 0) {
       AudioSource.PlayClipAtPoint(clips[Random.Range(0, clips.Length)], Camera.main.transform.position);
     }
---
+++ Assets/Interactables/Rotator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Rotator : MonoBehaviour {
+    private Rigidbody2D _rigidbody2D;
+
+    [SerializeField]
+    private float _angularVelocity = 100.0f;
+    
+    void Awake() {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    void LateUpdate() {
+        _rigidbody2D.angularVelocity = _angularVelocity;
+    }
+}
