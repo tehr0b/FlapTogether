f789114c9e2c266d9b467de84d43282b4e39fc29
Rob Giusti <therobgiusti@gmail.com>
2020-08-30 03:13:53 -0700

    Main menu improvements

--- Assets/MainMenu/MainMenu.cs
+++ Assets/MainMenu/MainMenu.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using Cysharp.Threading.Tasks;
 using JetBrains.Annotations;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
@@ -60,6 +61,8 @@ public class MainMenu : MonoBehaviour {
     var p3 = await WaitForNextKeyboardInput(2, p1, p2);
 
     _playerInputTextLabel.text = "Press Return to continue or Escape to return to the Main Menu.";
+    await UniTask.Yield();
+    LayoutRebuilder.ForceRebuildLayoutImmediate(_playerInputTextLabel.GetComponentInParent<Image>().rectTransform);
     
     var waitEnter = await WaitForSpecificKeyboardInput(KeyCode.Return, KeyCode.Escape);
     
@@ -81,23 +84,25 @@ public class MainMenu : MonoBehaviour {
 
   private async UniTask<KeyCode> WaitForNextKeyboardInput(int index, params KeyCode[] extraDisallows) {
     _playerInputTextLabel.text = $"Press the input key for player {index + 1}";
+    await UniTask.Yield();
+    LayoutRebuilder.ForceRebuildLayoutImmediate(_playerInputTextLabel.GetComponentInParent<Image>().rectTransform);
 
     while (true) {
-      if (Input.anyKey) {
+      if (Input.anyKeyDown) {
         for (var i = 0; i < (int) KeyCode.Joystick8Button19; i++) {
           if (_disallowedKeycodes.Contains((KeyCode) i) || extraDisallows.Contains((KeyCode) i)) {
             continue;
           }
 
-          if (Input.GetKey((KeyCode) i)) {
+          if (Input.GetKeyDown((KeyCode) i)) {
             _birdInputTexts[index].text = $"{(KeyCode) i}";
-            await UniTask.Delay(TimeSpan.FromMilliseconds(50));
+            await UniTask.Yield();
             return (KeyCode) i;
           }
         }
       }
 
-      await UniTask.Delay(TimeSpan.FromMilliseconds(50));
+      await UniTask.Yield();
     }
   }
 
