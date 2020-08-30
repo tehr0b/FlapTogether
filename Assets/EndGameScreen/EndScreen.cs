d5646b4e4b799ac5e85c0a4fe7746743c028ee3d
Rob Giusti <therobgiusti@gmail.com>
2020-08-30 03:36:50 -0700

    End screen

---
+++ Assets/EndGameScreen/EndScreen.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndScreen : MonoBehaviour {
+    [SerializeField]
+    private SceneReference _mainMenuScene;
+
+    private void Start() {
+        MusicManager.Instance.RequestTrack(MusicManager.Track.Menu);
+    }
+
+    [UsedImplicitly]
+    public void OnMainMenuPressed() {
+        SceneManager.LoadScene(_mainMenuScene);
+    }
+}
